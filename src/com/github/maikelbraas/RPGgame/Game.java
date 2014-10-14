cdd1303baa4f1ba3e9cca7ea42370826b79c302c
maikelbraas <maikebraas@gmail.com>
2014-10-14 12:28:41 +0200

    Source
    I uploaded the source of the game.

---
+++ src/com/github/maikelbraas/RPGgame/Game.java
@@ -0,0 +1,16 @@
+package com.github.maikelbraas.RPGgame;
+
+public class Game {
+	
+	
+	public Game ()
+	{
+		System.out.println("Yo");
+	}
+	
+	public static void main (String args[])
+	{
+		
+		new Game ();
+	}
+}
